2d6b6a69834cae028109d2f43217c8b14a436701
keki-sun <rabotovaosundy@gmail.com>
2026-06-23 09:28:55 +0300

    fin

--- src/main/java/com/example/servlet/FrontControllerServlet.java
+++ src/main/java/com/example/servlet/FrontControllerServlet.java
@@ -2,6 +2,11 @@ package com.example.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.annotation.Controller;
+import com.example.util.Utilitaire;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -9,7 +14,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontControllerServlet extends HttpServlet{
+
+    private List<String> listClass = new ArrayList<>();
     
+    @Override
+    public void init() throws ServletException{
+        try {
+            String packageName = getServletContext().getInitParameter("package");
+
+            List<Class<?>> classes = Utilitaire.getClassesAnnote(packageName, Controller.class);
+
+            for(Class<?> c : classes) {
+                listClass.add(c.getName());
+            }
+
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         processRequest(req,resp);
@@ -24,6 +47,15 @@ public class FrontControllerServlet extends HttpServlet{
         resp.setContentType("text/plain");
         String url = req.getRequestURL().toString();
         PrintWriter out = resp.getWriter();
-        out.println(url);       
+        out.println(url); 
+        
+        out.println("Classes with @Controller annotation:");
+        if(listClass.isEmpty()) {
+            out.println("No classes found with @Controller annotation.");
+        } else {
+            for(String className : listClass) {
+                out.println(className);
+            }
+        }
     }
 }
